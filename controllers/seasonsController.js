a91f90be1be06a32b9381f5a9bfad05b49c745c5
mike14747 <mike4747@oh.rr.com>
2021-02-07 22:41:50 -0500

    Set the starting year for scraping data to 1916. Added a res.setTimeout() to the repopulate endpoint of 10 minutes because the scraping will take longer than the default server timeout. The app is working in getting data from 1916 through 2020.

--- controllers/seasonsController.js
+++ controllers/seasonsController.js
@@ -7,17 +7,29 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/repopulate', async (req, res, next) => {
-    const startingSeason = 1990;
+    const startingSeason = 1916;
     const endingSeason = 2020;
     const seasonsArr = [];
 
+    const MS_IN_TEN_MINUTES = 60 * 10 * 1000;
+    res.setTimeout(MS_IN_TEN_MINUTES, () => {
+        return res.status().json({ message: `The respond timed out at ${MS_IN_TEN_MINUTES} seconds.` });
+    });
+
     try {
+        const NS_PER_SEC = 1e9;
+        const time = process.hrtime();
         for (let i = startingSeason; i <= endingSeason; i++) {
             const seasonData = await getSeasonErrors(i);
             seasonsArr.push(seasonData);
         }
+
+        // res.status(299).json(seasonsArr);
+
         const [data, error] = await Season.addNewData(seasonsArr);
-        data ? res.status(201).json({ message: `Successfully added ${data[1].affectedRows} new season of errors row(s) to the database!`, added: data[1].affectedRows }) : next(error);
+        const diff = process.hrtime(time);
+
+        data ? res.status(201).json({ message: `Successfully added ${data[1].affectedRows} new season(s) of errors row(s) to the database in ${((diff[0] * NS_PER_SEC + diff[1]) / NS_PER_SEC).toFixed(2)} seconds!`, added: data[1].affectedRows }) : next(error);
     } catch (error) {
         next(error);
     }
--- controllers/utils/repopulateFunctions.js
+++ controllers/utils/repopulateFunctions.js
@@ -10,12 +10,12 @@ const getSeasonErrors = async (season) => {
             positions.map(async (p) => {
                 const response = await axios.get(`https://www.baseball-reference.com/leagues/MLB/${season}-specialpos_${p}-fielding.shtml`);
                 const $ = cheerio.load(response.data);
-                innings = $('tfoot td[data-stat=Inn_def]').html();
+                if (p === 'p') innings = $('tfoot td[data-stat=Inn_def]').html();
                 const errors = $('tfoot td[data-stat=E_def]').html();
                 return errors ? parseInt(errors) : 0;
             }),
         );
-        return innings ? [season, parseFloat(innings.replace('.1', '.33').replace('.2', '.67')), ...allPositionErrors] : null;
+        return innings ? [season, parseFloat(innings.replace('.1', '.33').replace('.2', '.67')), ...allPositionErrors] : 0;
     } catch (error) {
         console.log(error);
     }
